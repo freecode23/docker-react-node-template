2bce3a4de44d52273577eb70ec657a5478d4baf9
sherly <sharton1@asu.edu>
2022-12-01 17:32:54 +0700

    build works

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { axiosInstance } from './config';
 function App() {
-  console.log(process.env.REACT_APP_GOOGLE_API);
+  console.log("api=",process.env.REACT_APP_GOOGLE_API);
   return (
     <div className="App">
       <button>Get Title</button>
