481155bf313a5d2629e6c7b52b9b882fbcbd1a4a
sherly <sharton1@asu.edu>
2022-12-01 16:41:46 +0700

    remove report webvitals and apptest.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import axios from "axios"
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/api/"
+    baseURL: "http://localhost:3050/api/"
 })
